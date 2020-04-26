37490d53b817b50e46b7acdd54a9c05463c016f4
Jonas-July <jonas.july@outlook.de>
2020-04-26 18:16:09 +0200

    Added header file for encryption and decryption
    As an authenticated symmetric cipher Deoxys takes in a key, a nonce, the message and some additional data. It encrypts the message and produces a tag based on the message and the additional data. The tag is used to authenticate this data after decryption.

---
+++ deoxys_II.h
@@ -0,0 +1,12 @@
+
+
+
+
+
+uint8_t* Deoxys_II_encrypt_buffer(	uint8_t* const key, uint8_t* const nonce,
+					uint8_t* const buffer_message, uint32_t buffer_message_size, 
+					uint8_t* const buffer_ad, uint32_t buffer_ad_size);
+
+void Deoxys_II_decrypt_buffer(	uint8_t* const key, uint8_t* const nonce, uint8_t* const tag, 
+				uint8_t* const buffer_ciphertext, uint32_t buffer_ciphertext_size, 
+				uint8_t* const buffer_ad, uint32_t buffer_ad_size);
