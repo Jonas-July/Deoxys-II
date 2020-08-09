48c4c109954016b2b1d74c87ceb92d131948c582
Jonas-July <jonas.july@outlook.de>
2020-08-09 20:34:17 +0200

    Changed signatures of encryption and decryption
    Changed constness of pointer to constness of content of the input arrays for encryption and decryption
    
    The input arrays shall be unchanged after en- and decryption. Whether their pointers where changed internally does not concern the outside user.

--- deoxys_II.h
+++ deoxys_II.h
@@ -1,12 +1,13 @@
+#include <stdint.h>
 
 
 
+uint8_t* Deoxys_II_encrypt_buffer(	uint8_t const* key, uint8_t const* nonce,
+					uint8_t const* buffer_message, uint32_t buffer_message_size, 
+					uint8_t const* buffer_ad, uint32_t buffer_ad_size);
 
+void Deoxys_II_decrypt_buffer(	uint8_t const* key, uint8_t const* nonce, uint8_t const* tag, 
+				uint8_t const* buffer_ciphertext, uint32_t buffer_ciphertext_size, 
+				uint8_t const* buffer_ad, uint32_t buffer_ad_size);
 
-uint8_t* Deoxys_II_encrypt_buffer(	uint8_t* const key, uint8_t* const nonce,
-					uint8_t* const buffer_message, uint32_t buffer_message_size, 
-					uint8_t* const buffer_ad, uint32_t buffer_ad_size);
-
-void Deoxys_II_decrypt_buffer(	uint8_t* const key, uint8_t* const nonce, uint8_t* const tag, 
-				uint8_t* const buffer_ciphertext, uint32_t buffer_ciphertext_size, 
-				uint8_t* const buffer_ad, uint32_t buffer_ad_size);
+uint8_t* Deoxys_BC_encrypt(uint8_t const* key, uint8_t const* tweak, uint8_t const* message);
